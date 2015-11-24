709ffd97e76d289ce75fe3ad5c9b7fb6aae3eefa
Ted Strzalkowski <tedski@luciddg.com>
2015-11-24 07:30:07 -0800

    adds ability to run wireshark locall

--- playground/pcap.sh
+++ playground/pcap.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
 # ssh config
-SSHCONFIG=$(mktemp)
+SSHCONFIG=$(mktemp /tmp/sshconfig.XXXX)
+vagrant ssh-config > $SSHCONFIG
 
 # make the named pipe
 # this is unsafe and i don't care
-PIPE=$(mktemp -u)
-mkfifo $PIPE
+PIPE=$(mktemp -u /tmp/pcap.XXXX)
+trap 'echo "Cleaning up.."; rm -rf "PIPE"' EXIT INT TERM HUP
+mkfifo -m 600 "$PIPE"
 
-ssh -F $SSHCONFIG 'sudo tcpdump -i any -s0 -w - "not port ssh" 2>/dev/null' > $PIPE
+echo "Starting Wireshark..."
+wireshark -k -o capture.pcap_ng:FALSE -i $PIPE >/dev/null 2>&1 &
+
+echo "Starting remote capture..."
+echo "Use ^C to exit."
+ssh -F $SSHCONFIG default 'sudo tcpdump -l -i h1-eth1 -s0 -w - "not port ssh" 2>/dev/null' > $PIPE
